28a4bd3722bee46e16f29bcffce3be52b6a2c8ec
utkarshdagoat <utkarsh382004@gmail.com>
2024-03-10 15:58:53 +0530

    completes provider cli

--- provider/src/provider.rs
+++ provider/src/provider.rs
@@ -24,7 +24,7 @@ pub fn add_provider_to_network(storage: u32, ram: u32, cpu_cores: u32) {
         ip: utils::get_local_ip().unwrap(),
     };
 
-    let root = Node::new(rootNodeInfo.ip, 7999, rootNodeInfo.clone());
+    let root = Node::new(rootNodeInfo.ip.clone(), 7999, rootNodeInfo.clone());
 
     let node = Node::new(utils::get_local_ip().unwrap(), 63001, nodeInfo.clone());
     let interface = Protocol::new(node.ip, node.port, node.info, Some(root.clone()));
@@ -32,7 +32,7 @@ pub fn add_provider_to_network(storage: u32, ram: u32, cpu_cores: u32) {
     interface.put_attributes("ram".to_string(), interface.node.info.ram);
     interface.put_attributes("virtual_cpu".to_string(), interface.node.info.cpu_cores);
     interface.put_attributes("arm_image".to_string(), interface.node.info.arch_images);
-    interface.put_tuple(interface.node.id, interface.node.info);
+    interface.put_tuple(interface.node.id, interface.node.info.clone());
 
     let ten_millis = time::Duration::from_millis(10);
     loop {
