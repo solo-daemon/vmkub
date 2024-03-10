59fc1e8bd16cf01dd18b5e5d438a09f1a1829105
utkarshdagoat <utkarsh382004@gmail.com>
2024-03-10 17:20:17 +0530

    adds client cli

---
+++ client/cli/src/lib.rs
@@ -0,0 +1,25 @@
+use kademlia_dht::node::{Node, NodeInfo};
+use kademlia_dht::protocol::Protocol;
+use kademlia_dht::utils;
+
+pub fn add_client_to_network(storage: u32, ram: u32, cpu_cores: u32) -> Protocol {
+    let rootNodeInfo = NodeInfo {
+        storage: 100,
+        ram: 8,
+        cpu_cores: 2,
+        arch_images: 0,
+        ip: "10.11.1.59".to_string(),
+    };
+    let nodeInfo = NodeInfo {
+        storage,
+        ram,
+        cpu_cores,
+        arch_images: 0,
+        ip: utils::get_local_ip().unwrap(),
+    };
+
+    let root = Node::new(rootNodeInfo.ip.clone(), 7999, rootNodeInfo.clone());
+
+    let node = Node::new(utils::get_local_ip().unwrap(), 63001, nodeInfo.clone());
+    Protocol::new(node.ip, node.port, node.info, Some(root.clone()))
+}
--- client/cli/src/main.rs
+++ client/cli/src/main.rs
@@ -1,26 +1,57 @@
-mod connect;
+use console::Emoji;
+use kademlia_dht::attributes::Query;
+use std::io;
+static SPARKLE: Emoji<'_, '_> = Emoji("✨ ", ":-)");
+static WORLD: Emoji<'_, '_> = Emoji("🌐", "");
+pub mod lib;
 
-use connect::connect_client;
-
-fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    
-    if args.len() < 2 {
-        println!("Usage: {} <command> [args]", args[0]);
-        println!("Available commands:");
-        println!("  connect <path>");
-        return;
-    }
-
-    let command = &args[1];
-    match command.as_str() {
-        "connect" => connect_client(&args[2..]),
-        _ => {
-            println!("Unknown command: {}", command);
-            println!("Usage: {} <command> [args]", args[0]);
+fn get_integer_input() -> u32 {
+    loop {
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => match input.trim().parse::<u32>() {
+                Ok(value) => return value,
+                Err(_) => println!("Please enter a valid integer."),
+            },
+            Err(error) => {
+                eprintln!("Error reading input: {}", error);
+                std::process::exit(1);
+            }
         }
     }
 }
 
+fn main() {
+    println!("Welcome to Our Network! {}", WORLD);
+    println!(
+        "{} Thank you for considering our network for your requirements.",
+        SPARKLE
+    );
+    println!("We're eager to cater to your needs and provide top-notch services.");
 
+    println!("To begin, please provide the following details:");
 
+    println!("Please specify the amount of RAM you need (in GB):");
+
+    let ram: u32 = get_integer_input();
+    println!("Please indicate the desired number of CPU cores:");
+
+    let cpu_cores: u32 = get_integer_input();
+    println!("Please enter the required storage capacity (in GB):");
+
+    let storage: u32 = get_integer_input();
+
+    println!("Requesting network changes to accommodate your specifications...");
+    let interface = lib::add_client_to_network(storage, ram, cpu_cores);
+    let req = Query {
+        storage: interface.node.info.storage,
+        ram: interface.node.info.ram,
+        cpu_cores: interface.node.info.cpu_cores,
+        arch_images: interface.node.info.arch_images,
+    };
+    let best_fit = interface.get_best_fit(req);
+    dbg!(best_fit);
+    println!(
+        "You're now equipped to access and benefit from our network resources. Welcome aboard!"
+    );
+}
